bba609f93805b1f5a354b814b6e336432e773c9d
Gandalf91 <89839990+Gandalf91@users.noreply.github.com>
2023-04-26 19:51:51 +0400

    nodddem

--- dexBack/index.js
+++ dexBack/index.js
@@ -11,7 +11,7 @@ const ABI = require('./abi.json')
 app.use(cors())
 app.use(express.json())
 
-//app.use(express.static(path.join(__dirname + '/public')))
+//app.use(express.static(path.join(__dirname + '/public'))
 
 //const PORT = process.env.PORT || 3001
 //app.listen(PORT)
