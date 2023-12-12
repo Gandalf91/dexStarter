ef1eee69e7a990be41a6ced8304f87ad35568b1d
Gandalf91 <89839990+Gandalf91@users.noreply.github.com>
2023-12-12 04:35:57 +0400

    hgusdnjddkrrr

--- dex/src/components/Dashboard.js
+++ dex/src/components/Dashboard.js
@@ -70,10 +70,10 @@ function Dashboard({ dollars, balance, rewards, getNameAndBalance }) {
         </div>
 
         <div className="balanceOptionsD">
-          <div className="extraOptionD">My Balance: 260 Matic</div>
+          <div className="extraOptionD">My Balance: 910 Matic</div>
         </div>
         <div className="balanceOptionsD">
-          <div className="extraOptionD"> Balance in USD: 213$</div>
+          <div className="extraOptionD"> Balance in USD: 764$</div>
         </div>
         {/* <div className="balanceOptionsD">
           <div className="extraOptionD">My Rewards: {rewards} Matic</div>
