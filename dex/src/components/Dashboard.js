1a9c2e8618ce3c67a399f28cf663e164175a7aba
Gandalf91 <89839990+Gandalf91@users.noreply.github.com>
2024-08-23 09:52:18 +0400

    fvhgklvyb

--- dex/src/components/Dashboard.js
+++ dex/src/components/Dashboard.js
@@ -70,13 +70,11 @@ function Dashboard({ dollars, balance, rewards, getNameAndBalance }) {
         </div>
 
         <div className="balanceOptionsD">
-          <div className="extraOptionD">My Balance: {balance} 14860 Matic</div>
+          <div className="extraOptionD">My Balance: {balance} Matic</div>
         </div>
         <div className="balanceOptionsD">
-          <div className="extraOptionD"> Balance in USD: {dollars}6105$</div>
+          <div className="extraOptionD"> Balance in USD: {dollars}$</div>
         </div>
-        <div>Both addr cleared: Ready to withdraw</div>
-        <div>Accum fees: 16 matic</div>
 
         {/* <div className="balanceOptionsD">
           <div className="extraOptionD">My Rewards: {rewards} Matic</div>
